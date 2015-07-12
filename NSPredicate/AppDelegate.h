42ea2ee01cae73ab49192816b0d2d2c52b6f4840
哲人蔡 <philcai@yeah.net>
2015-07-12 23:48:26 +0800

    Initial Commit

---
+++ NSPredicate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSPredicate
+//
+//  Created by 哲人蔡 on 15/7/12.
+//  Copyright (c) 2015年 Phil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
